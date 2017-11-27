a9461697dc22b73ce8215f231debdfcfba010de3
wanwenqq <wanwenqq@hotmail.com>
2017-11-27 14:31:00 +0800

    scrapy test
    Signed-off-by: wanwenqq <wanwenqq@hotmail.com>

---
+++ les1/attr.py
@@ -0,0 +1,16 @@
+
+#coding   = utf-8
+
+class waththefuckday(object):
+    a = 0
+
+
+a = waththefuckday()
+b = waththefuckday()
+
+
+a.a = 1
+print (a.a)
+print (b.a)
+print(id(a.a))
+print(id(b.a))
