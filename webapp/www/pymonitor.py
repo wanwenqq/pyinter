7f453245b0d4e3452f5337d76fb449176e84801c
wanwenqq <wanwenqq@hotmail.com>
2018-04-18 09:09:45 +0800

    doubanyuedu

--- webapp/www/pymonitor.py
+++ webapp/www/pymonitor.py
@@ -56,7 +56,7 @@ def start_watch(path, callback):
             time.sleep(0.5)
     except KeyboardInterrupt:
         observer.stop()
-    observer.join()
+    observer.join() observer.join()
 
 if __name__ == '__main__':
     argv = sys.argv[1:]
